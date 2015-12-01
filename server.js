f0a512834e170ce2d49f0e65ed907018e12e3e79
Steve Hollaar <steve.hollaar@gmail.com>
2015-12-01 13:04:30 -0800

    Use port from env var

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ import express from 'express';
 import moment from 'moment';
 
 const app = express();
+const {PORT} = process.env;
+
 app.get('/', (req, res) => {
   console.log('got request');
   const date = moment().format();
   res.send(`Hello world ${date}`);
 });
 
-const server = app.listen(8081, () => {
+const server = app.listen(PORT, () => {
   const {port} = server.address();
 
   console.log(`Listening on port ${port}`);
