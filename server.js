6e2c84b8ddbd686790bc304e840ba244f2ee9cbb
Steve Hollaar <steve.hollaar@gmail.com>
2016-04-01 22:38:01 -0700

    Update deps, respond with etsy redirect, disable rethinkdb & redis

--- server.js
+++ server.js
@@ -1,23 +1,29 @@
 import express from 'express';
 import moment from 'moment';
-import redis from 'redis';
-import r from 'rethinkdb';
+// import redis from 'redis';
+// import r from 'rethinkdb';
 
 const app = express();
-const redisClient = redis.createClient();
+// const redisClient = redis.createClient();
 const {PORT} = process.env;
-r.connect({host: 'localhost', port: 28015}, (err, conn) => {
-  if (err) throw err;
-  console.log('Connected to rethinkdb');
-});
+// r.connect({host: 'localhost', port: 28015}, (err, conn) => {
+//   if (err) throw err;
+//   console.log('Connected to rethinkdb');
+// });
 
-redisClient.on('error', console.error);
+// redisClient.on('error', console.error);
+//
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   console.log('got request');
-  const date = moment().format();
-  res.send(`Hello world ${date}`);
+  res.redirect('https://www.etsy.com/shop/InkLaneDesign');
 });
+
+// app.get('/', (req, res) => {
+//   console.log('got request');
+//   const date = moment().format();
+//   res.send(`Hello world ${date}`);
+// });
 
 const server = app.listen(PORT, () => {
   const {port} = server.address();
