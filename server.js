79a64b776fb5257bee04a4d791dd030c1c966853
Steve Hollaar <steve.hollaar@gmail.com>
2017-03-12 21:04:23 -0700

    Cleanup, upgrade deps

--- server.js
+++ server.js
@@ -1,35 +1,16 @@
 import express from 'express';
-import moment from 'moment';
 import morgan from 'morgan';
-// import redis from 'redis';
-// import r from 'rethinkdb';
 
 const app = express();
-// const redisClient = redis.createClient();
 const { PORT = 9000 } = process.env;
 
 app.use(morgan('combined')); // HTTP logging middleware.
-
-// r.connect({host: 'localhost', port: 28015}, (err, conn) => {
-//   if (err) throw err;
-//   console.log('Connected to rethinkdb');
-// });
-
-// redisClient.on('error', console.error);
-//
 
 app.get('*', (req, res) => {
   res.redirect('https://www.etsy.com/shop/InkLaneDesign');
 });
 
-// app.get('/', (req, res) => {
-//   console.log('got request');
-//   const date = moment().format();
-//   res.send(`Hello world ${date}`);
-// });
-
 const server = app.listen(PORT, () => {
   const {port} = server.address();
-
   console.log(`Listening on port ${port}`);
 });
